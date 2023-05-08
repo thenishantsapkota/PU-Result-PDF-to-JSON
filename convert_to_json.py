f80c6f3aa7f3d1c3517a8ff966919951baae73f4
Nishant Sapkota <hello@snishant.com.np>
2023-05-08 18:01:04 +0545

    feat: use pdfplumber instead of tabula
    Now this reads files more efficiently

--- convert_to_json.py
+++ convert_to_json.py
@@ -3,52 +3,56 @@ import json
 import os
 
 import pandas as pd
-import tabula
+import pdfplumber
 
 directory = "./results"
 files = os.listdir(directory)
 
 
 def convert_to_json(filename):
+    json_dir = "./json"
+    if not os.path.isdir(json_dir):
+        os.makedirs(json_dir)
+
     try:
-        if not os.path.isdir("./json"):
-            os.system("mkdir json")
-
-        tables = tabula.read_pdf(
-            f"./results/{filename}", pages="all", pandas_options={"dtype": str}
-        )
-
-        buf = io.BytesIO()
-
-        df = pd.concat(tables)
-
-        df.columns = [col.replace("\n", "").replace("\r", " ") for col in df.columns]
-
-        df.dropna(how="all", inplace=True)
-
-        df.to_csv(buf, index=False)
-
-        buf.seek(0)
-        # Converting to JSON
-        csv_df = pd.read_csv(buf)
-        csv_df.set_index("Exam Roll No.", inplace=True)
+        with pdfplumber.open(f"./results/{filename}") as pdf:
+            tables = []
+            for page in pdf.pages:
+                page_tables = page.extract_tables()
+                for table in page_tables:
+                    tables.append(table)
 
         result = {}
-        for roll_no, row in csv_df.iterrows():
-            roll_dict = {}
-            for col_name, value in row.items():
-                if col_name == "Exam Roll No.":
-                    continue
-                snake_case_name = col_name.lower().replace(" ", "_")
-                if pd.isna(value):
-                    value = "-"
-                roll_dict[snake_case_name] = value
-            result[str(roll_no)] = roll_dict
-        with open(f"./json/{filename.split('.')[0]}.json", "w") as f:
+        for table in tables:
+            buf = io.StringIO()
+            df = pd.DataFrame(table[1:], columns=table[0])
+            df.columns = [
+                col.replace("\n", " ").replace("\r", " ") for col in df.columns
+            ]
+            df.dropna(how="all", inplace=True)
+            df.to_csv(buf, index=False)
+            buf.seek(0)
+
+            # Converting to JSON
+            csv_df = pd.read_csv(buf, dtype=str)
+            csv_df.set_index("Exam Roll No.", inplace=True)
+
+            for roll_no, row in csv_df.iterrows():
+                roll_dict = {}
+                for col_name, value in row.items():
+                    if col_name == "Exam Roll No.":
+                        continue
+                    snake_case_name = col_name.lower().replace(" ", "_")
+                    if pd.isna(value):
+                        value = "-"
+                    roll_dict[snake_case_name] = value
+                result[str(roll_no)] = roll_dict
+
+        with open(f"{json_dir}/{filename.split('.')[0]}.json", "w") as f:
             json.dump(result, f, indent=4)
-    except Exception:
-        print(f"Unable to convert {filename}")
-        pass
+
+    except Exception as e:
+        print(f"Unable to convert {filename}: {e}")
 
 
 for filename in files:
